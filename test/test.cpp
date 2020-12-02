60a90c6afbb0c6bbb887ce6ca5d27748c89544d0
z.shmaria <z.shmaria@auto-i-dat.ch>
2020-12-02 21:58:11 +0100

    general wip

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "../include/papiwrapper.h"
 
 int main()
 {
-    std::cout << "Hello World!";
+    PapiWrapper wpapi;
+    wpapi.Init();
+    wpapi.AddEvent(PAPI_FSQ_INS_idx);
+    std::cout << "Hello World!" << std::endl;
 }
